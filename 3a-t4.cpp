c8756e69761d0bea3ef12b5e0b0af8149880c617
Danikich <95334660+Danikich@users.noreply.github.com>
2021-12-01 09:21:43 +0300

    Update 3a-t4.cpp
    Added some check statements for integers

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,4 +1,3 @@
-
 #include<iostream> 
 using namespace std;
 
@@ -7,15 +6,24 @@ int main()
 	int k, i = 1, sum_k = 0, number;
 	cout << " Enter the amount of integers to sum = "; cin >> k;
 	do {
-		if (k == 0)
-		{
-			break;
-		}
 		cout << "Enter integer nr. " << i << ": ";
 		cin >> number;
+		while (1)
+		{
+			if (cin.fail())
+			{
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				cout << "You have entered wrong input, write another integer number: " ;
+				cin >> number;
+			}
+			if (!cin.fail())
+				break;
+		}
 		sum_k += number;
 		i++;
 	} while (i <= k);
 	cout << " The total sum of " << k << " integers is: " << sum_k;
 	return 0;
 }
+
